2e3473251db45ab5caec7db68ed7ee7ed35243fb
schaefes <86061473+schaefes@users.noreply.github.com>
2026-09-14 16:43:24 +0200

    Update L1T SC4NG Jet Model Path to version 1.0.1

--- NtupleProducer/python/runPerformanceNTuple.py
+++ NtupleProducer/python/runPerformanceNTuple.py
@@ -341,7 +341,7 @@ def addNNPuppiTaus():
 
 def addMultitagging(): #extended TRK
     classes = process.l1tSC4NGJetProducer.classes
-    #process.l1tSC4NGJetProducer.l1tSC4NGJetModelPath = cms.string(os.environ['CMSSW_BASE']+"/src/L1TSC4NGJetModel/L1TSC4NGJetModel_v0")
+    process.l1tSC4NGJetProducer.l1tSC4NGJetModelPath = cms.string(os.environ['CMSSW_BASE']+"/src/L1TSC4NGJetModel/L1TSC4NGJetModel_v1_0_1")
     for i in range(8):
         setattr(process.l1pfjetTaggerTable.moreVariables, "tagScore_%s" % (classes[i]), cms.string("getTagScores()[%s]"  % (i)))
 
